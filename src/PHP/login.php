1991da1b116b420ae459b049225f480618106041
RetroRobinChaos7 <robingraham1204@gmail.com>
2025-03-10 14:18:39 +0200

    Database connection and login/registration with db

--- src/PHP/login.php
+++ src/PHP/login.php
@@ -5,11 +5,40 @@ if ($_SERVER["REQUEST_METHOD"] === "POST"){
     $cPass = htmlspecialchars($_POST["cpassword"]);
 
     //temporary login until i can get a databsase up and running on this guy
-    if ($uName === "Retro" && $pass === "retropaswoord7" && $cPass === $pass || !empty($uName) || !empty($pass) || !empty($cPass)){
-        header("Location: ../index.html");
+    if ($cPass === $pass && !empty($uName) && !empty($pass) && !empty($cPass)){
+        $conn = new mysqli('localhost','root','','userbase_db');
+        if ($conn->connect_error) {
+            die('Connection failed : '. $conn->connect_error);
+        }else{
+            $stmt = $conn->prepare('Select password from users where username = ?');
+            $stmt->bind_param("s",$$uName);
+            if($stmt->execute()){
+                $stmt-> bind_result($storedpassword);
+                if(password_verify($pass,$storedpassword)) {
+                    echo"<script>
+                        alert('Login Successfull!');
+                        window.location.href = '../index.html';
+                        </script>";
+                }else{
+                    echo "<script>
+                        alert('Password doesnt match!');
+                        setTimeout(function() { window.location.href = 'login.php'; }, 2000);
+                        </script>";
+                }
+            }else{
+                echo "<script>
+                    alert('Username does'nt match!');
+                    setTimeout(function() { window.location.href = 'login.php'; }, 2000);
+                    </script>";
+            }            
+            $stmt->close();
+            $conn->close();
+        }
     }else{
-        header("Location: ../Pages/login.html");
-        exit();
+        echo "<script>
+            alert('Please fill all fields and ensure passwords match!');
+            setTimeout(function() { window.location.href = '../Pages/login.html'; }, 2000);
+        </script>";
     }
 }else{
     header("Location: ../Pages/login.html");
--- src/PHP/register.php
+++ src/PHP/register.php
@@ -6,10 +6,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST"){
     $pass = htmlspecialchars($_POST["password"]);
     $cPass = htmlspecialchars($_POST["cpassword"]);
 
-    if (empty($email)||empty($uName)||empty($dob)||empty($pass)||empty($cPass)){
+    if (empty($email)||empty($uName)||empty($dob)||empty($pass)||empty($cPass) && $cPass === $pass){
         header("Location: ../Pages/register.html");
         exit();
     }else{
+        include "userDB/connection.php";
         header("Location: ../index.html");
     }
 }else{
