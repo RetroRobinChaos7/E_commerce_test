1fce449492e8a4d0c3f01b0086d0f9afb61a77cf
RetroRobinChaos7 <robingraham1204@gmail.com>
2025-05-07 22:30:24 +0200

    Added a working way to add you list to the kist databse

--- src/PHP/add.php
+++ src/PHP/add.php
@@ -1,17 +1,38 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 session_start();  
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $title = htmlspecialchars($_POST['title']);
     $price = htmlspecialchars($_POST['price']);
     $cat = htmlspecialchars($_POST['cat']);
     $desc = htmlspecialchars($_POST['desc']);
-    $listImg = htmlspecialchars($_POST['listImg']); 
     $username = $_SESSION['username'];
 
-    include 'mysqlConn.php';
-    $imageLikn = "";
-    $stmt = $conn->prepare('Insert Into listdb(title,price,cat,description,itemImage,user)');
-    $stmt->bind_param('sdssbs', $title, $price, $cat, $desc, $listImg, $username);
+    include '../PHP/userDB/mysqlConn.php';
+    
+    $imageDir = "../Images/Uploads/";
+    $imageName = basename($_FILES["listImg"]["name"]);
+    $imageType = strtolower(pathinfo($imageName, PATHINFO_EXTENSION));
+    $uniqueName = uniqid().".".$imageType;
+    $targetFile = $imageDir.$uniqueName;
+
+    $allowedTypes = ['image/jpeg','image/png'];
+    $fileType = mime_content_type($_FILES["listImg"]["tmp_name"]);
+
+    if(!in_array($fileType,$allowedTypes)){
+        echo "<script>alert('only JPG and PNG file are allowed for images!')</script>";
+        exit();
+    }
+    if(move_uploaded_file($_FILES["listImg"]["tmp_name"], $targetFile)) {
+        $listImg = "Images/Uploads/". $uniqueName;
+    }else {
+        echo "<script>alert('The file failed to upload!')</script>";
+        exit();
+    }
+
+    $stmt = $conn->prepare('Insert Into listdb(title,price,cat,description,itemImage,user) values (?,?,?,?,?,?)');
+    $stmt->bind_param('sdssss', $title, $price, $cat, $desc, $listImg, $username);
     if($stmt->execute()){
         echo "<script>
                     alert('Item Listed!');
@@ -23,5 +44,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     }
     $stmt->close();
     $conn->close();
+
     header('location: ../Pages/Listings/list.html');
+    exit();
 }
