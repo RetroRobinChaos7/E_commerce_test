948f89bbcbf782bf5c94701f6a548291d42dfa2f
RetroRobinChaos7 <robingraham1204@gmail.com>
2025-06-05 14:05:32 +0200

    Adding composer and updated configs

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'retro/matexchange',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8d8c3f018b5d72587d61e07039f01427f9b2214c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'retro/matexchange' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8d8c3f018b5d72587d61e07039f01427f9b2214c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
