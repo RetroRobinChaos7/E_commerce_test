ac847e2cf88140142ace133d2c5ab5f7e0976f0c
RetroRobinChaos7 <robingraham1204@gmail.com>
2025-06-05 13:37:54 +0200

    Fixed database connection

--- src/PHP/userDB/mysqlConn.php
+++ src/PHP/userDB/mysqlConn.php
@@ -1,13 +1,12 @@
 <?php
+$host = getenv('DB_HOST');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_NAME');
+$port = getenv('DB_PORT') ?: '3306';
 
-$servername = "localhost";
-$dbUsername = "root";
-$password = "";
-$datebaseName = "userbase_db";
-
-$conn = new mysqli($servername, $dbUsername, $password, $datebaseName);
-
+$conn = new mysqli($host, $username, $password, $database, $port);
 if ($conn->connect_error) {
-    die("Connection Failed". $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 ?>
